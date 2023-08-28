607ab53f7317002e604044fee293fa3feb7622ef
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2023-08-28 19:18:32 +0200

    First exercise: variables and constants

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+const STARTING_MISSILES: i32 = 10;
+const READY_AMOUNT: i32 = 6;
+
 fn main() {
     println!("Rust 101 - Demo application");
+    let mut missiles = STARTING_MISSILES;
+    let ready: i32 = READY_AMOUNT;
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    missiles = missiles - ready;
+    println!("{} Missiles left", missiles);
 }
